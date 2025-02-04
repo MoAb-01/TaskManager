4021eea8b01800b5d0efce9f3844b53f41f9fcb3
Mohamed Abdou <111701730+MoAb-01@users.noreply.github.com>
2025-02-04 22:34:42 +0300

    Add files via upload

---
+++ TaskManager.cs
@@ -0,0 +1,30 @@
+public class TaskManager
+{
+                                        //applying composition principle
+    List<Task> tasklist= new List<Task>(); //created an object from the previous class and exploit methods to it
+    public List<Task> Tasklist //getters and setters does Return to the world or take anything as well only each variable interactable
+    {
+        get {return tasklist;} //shows the field variable to the world
+        set {tasklist=value;} //saves the value in the field vairable
+
+    }
+
+    public void AddTask (Task atask)
+    {
+        tasklist.Add(atask);
+    }
+    public void RemoveTask(Task atask)
+    {
+        tasklist.Remove(atask);
+    }
+    public void DisplayTasks()
+    {
+        foreach(var atask in tasklist)
+        {
+            Console.WriteLine(atask);
+        }
+        
+    }
+    
+
+}
